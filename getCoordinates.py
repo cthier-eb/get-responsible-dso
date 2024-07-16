e0cda83cc2e70b1c9c8eff14849c1bf3e4eeee8e
cthier-eb <cthier@e-bridge.com>
2024-07-17 01:06:23 +0200

    Updated getCoordinates

--- getCoordinates.py
+++ getCoordinates.py
@@ -6,8 +6,29 @@ import logging
 import argparse
 from pyproj import Proj, Transformer
 import random
+from nordvpn_switcher import initialize_VPN, rotate_VPN, terminate_VPN
+
+
+vpn_settings = initialize_VPN()
 
 # %%
+
+
+def get_random_user_agent():
+    user_agents = [
+        # List of various user agents
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (Linux; Android 11; Pixel 4a) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+    ]
+    return random.choice(user_agents)
 
 
 def convert_to_regular_coordinates(x, y, zone=32, ellps="WGS84"):
@@ -111,11 +132,12 @@ def get_query_selector():
 
 
 # %%
-def execute_query(variables):
+def execute_query(variables, retry_count=0, max_retries=5):
     url = "https://www.vnbdigital.de/gateway/graphql"
     headers = {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "User-Agent": get_random_user_agent()
     }
 
     query = get_query_selector()
@@ -124,11 +146,34 @@ def execute_query(variables):
         "variables": variables
     }
 
-    response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return False
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return False
+    except Exception as e:
+        if retry_count < max_retries:
+            print(f"Failed with exception: {e}")
+            print(f"Retrying after rotating VPN")
+
+            # Rotate the VPN
+            if retry_count % 2 == 0:
+                print("Sleeping for 30 seconds before rotating VPN")
+                # Disconnect VPN
+                terminate_VPN(vpn_settings)
+                time.sleep(30)
+            try:
+                rotate_VPN(vpn_settings)
+            except Exception as e:
+                print(f"Failed to rotate VPN with exception: {e}")
+                return False
+            # Try again with incremented retry_count
+            return execute_query(variables, retry_count + 1, max_retries)
+        else:
+            # Exceeded maximum retries
+            print(f"Failed after {max_retries} retries.")
+            return False
 
 
 if __name__ == "__main__":
@@ -137,7 +182,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Get the coordinates from the VNB Digital")
     parser.add_argument("--file", help="The file with the coordinates",
-                        default="grid_connection_points.xlsx")
+                        default="grid_connection_points_original.xlsx")
     parser.add_argument("--loglevel", help="The log level", default="INFO")
 
     # Parse the arguments
@@ -155,16 +200,23 @@ if __name__ == "__main__":
     # The coordinates are in column Longiture and Latitude
     # We will iterate over the rows and get the coordinates
     i = 1
-    dataSet = {}
+    with open("result.json", "r", encoding="utf-8") as f:
+        dataSet = json.load(f)
 
-    data['Coordinates'] = data.apply(lambda row: f"{convert_to_regular_coordinates(
-        row['Longitude'], row['Latitude'])}", axis=1)
+    # dataSet = {}
+
+    data['Coordinates'] = data.apply(
+        lambda row: f"{convert_to_regular_coordinates(row['Longitude'], row['Latitude'])}", axis=1)
 
     data["vnb-digital"] = ""
     data["voltage-levels"] = ""
+    rotate_VPN(vpn_settings)
 
     for index, row in data.iterrows():
-        print(i)
+        if (row["id"] in dataSet):
+            i += 1
+            print(f"Already processed {row['id']}")
+            continue
         lon = row["Longitude"]
         lat = row["Latitude"]
         # print(lon, lat)
@@ -185,6 +237,8 @@ if __name__ == "__main__":
         }
         result = execute_query(variables)
 
+        print(i)
+
         if (result):
             vnbs = []
             vnb_names = []
@@ -204,7 +258,7 @@ if __name__ == "__main__":
                     "types": vnb["types"],
                 }
                 vnbs.append(current_vnb)
-                voltage_levels.append(f"{vnb["voltageTypes"]}")
+                voltage_levels.append(f"{vnb['voltageTypes']}")
 
             row["vnb-digital"] = ", ".join(vnb_names)
             row["voltage-levels"] = ", ".join(voltage_levels)
@@ -213,9 +267,10 @@ if __name__ == "__main__":
             data.at[index, "voltage-levels"] = row["voltage-levels"]
             dataSet[row["id"]] = vnbs
 
-        if (i % 2 == 0):
-            with open("result.json", "w", encoding="utf-8") as f:
-                json.dump(dataSet, f, ensure_ascii=False, indent=4)
+        with open("result.json", "w", encoding="utf-8") as f:
+            json.dump(dataSet, f, ensure_ascii=False, indent=4)
+        if (i % 50 == 0):
+            rotate_VPN(vpn_settings)
 
             # Write the dataframe to an updated file
             with pd.ExcelWriter("updatedData.xlsx", engine="openpyxl") as writer:
@@ -223,11 +278,15 @@ if __name__ == "__main__":
 
         # print(convert_to_regular_coordinates(lon, lat))
         i += 1
-        if i == 20:
-            url = f"https://www.google.de/maps/@{x},{y},15z"
-            print(url)
-            break
-
         # Get random sleep time between 1 and 5 seconds
-        sleep_time = 1 + (4 * random.random())
+        sleep_time = 0 + (2*random.random())
+        if (i % 20 == 0):
+            time.sleep(4 + (2*random.random()))
         time.sleep(sleep_time)
+
+    with open("result.json", "w", encoding="utf-8") as f:
+        json.dump(dataSet, f, ensure_ascii=False, indent=4)
+
+    # Write the dataframe to an updated file
+    with pd.ExcelWriter("finalData.xlsx", engine="openpyxl") as writer:
+        data.to_excel(writer, index=False)
